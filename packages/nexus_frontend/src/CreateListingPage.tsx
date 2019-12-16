44b16ee3984ca1b4860cfd1f519cd8eb55e9f390
Zak Strassberg <zakstrassberg@gmail.com>
2019-12-15 19:36:04 -0500

    apollo refactor

---
+++ packages/nexus_frontend/src/CreateListingPage.tsx
@@ -0,0 +1,35 @@
+import React, { FunctionComponent } from "react";
+
+import { Formik, Form, Field } from "formik";
+
+const CreateListingPage: FunctionComponent = () => {
+  return (
+    <Formik
+      initialValues={{
+        name: "",
+        description: ""
+      }}
+      onSubmit={variables => {}}
+    >
+      <Form style={{ display: "flex", flexDirection: "column", maxWidth: 600 }}>
+        <fieldset>
+          <label style={{ display: "flex", flexDirection: "column" }}>
+            <span>Name</span>
+            <Field type="text" name="name" placeholder="Game Name" />
+          </label>
+          <label style={{ display: "flex", flexDirection: "column" }}>
+            <span>Description</span>
+            <Field
+              component="textarea"
+              name="description"
+              placeholder="Description"
+            />
+          </label>
+          <button type="submit">Create Listing</button>
+        </fieldset>
+      </Form>
+    </Formik>
+  );
+};
+
+export default CreateListingPage;
--- packages/nexus_frontend/src/LazyApp.tsx
+++ packages/nexus_frontend/src/LazyApp.tsx
@@ -1,32 +1,25 @@
 import React, { FunctionComponent } from "react";
-import { Formik, Form, Field } from "formik";
+import CreateListingPage from "./CreateListingPage";
+
+import {
+  ApolloClient,
+  HttpLink,
+  InMemoryCache,
+  ApolloProvider
+} from "@apollo/client";
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: "https://48p1r2roz4.sse.codesandbox.io"
+  })
+});
+
 const LazyApp: FunctionComponent = () => {
   return (
-    <Formik
-      initialValues={{
-        name: "",
-        description: ""
-      }}
-      onSubmit={variables => {}}
-    >
-      <Form style={{ display: "flex", flexDirection: "column", maxWidth: 600 }}>
-        <fieldset>
-          <label style={{ display: "flex", flexDirection: "column" }}>
-            <span>Name</span>
-            <Field type="text" name="name" placeholder="Game Name" />
-          </label>
-          <label style={{ display: "flex", flexDirection: "column" }}>
-            <span>Description</span>
-            <Field
-              component="textarea"
-              name="description"
-              placeholder="Description"
-            />
-          </label>
-          <button type="submit">Create Listing</button>
-        </fieldset>
-      </Form>
-    </Formik>
+    <ApolloProvider client={client}>
+      <CreateListingPage />
+    </ApolloProvider>
   );
 };
 

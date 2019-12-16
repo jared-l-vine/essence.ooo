f0ca78c98789b001ccc5d3556e4e3577380f8dab
Zak Strassberg <zakstrassberg@gmail.com>
2019-12-15 20:15:23 -0500

    terraform table

--- packages/terraform/nexus/dynamodb.tf
+++ packages/terraform/nexus/dynamodb.tf
@@ -1,1 +1,39 @@
 # TABLES GO HERE
+resource "aws_dynamodb_table" "nexus_listing_table" {
+  name           = "NexusListings"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "Id"
+
+  attribute {
+    name = "Id"
+    type = "S"
+  }
+
+  attribute {
+    name = "Title"
+    type = "S"
+  }
+
+  tags = {
+    App         = "Nexus"
+    Site        = "essence.ooo"
+  }
+}
+
+# resource "aws_dynamodb_table" "nexus_users_table" {
+#   name           = "NexusUsers"
+#   billing_mode   = "PAY_PER_REQUEST"
+#   hash_key       = "Id"
+#   attribute {
+#     name = "Id"
+#     type = "S"
+#   }
+#   attribute {
+#     name = "Title"
+#     type = "S"
+#   }
+#   tags = {
+#     App         = "Nexus"
+#     Site        = "essence.ooo"
+#   }
+# }

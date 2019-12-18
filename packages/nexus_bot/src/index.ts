927863304580185e5bcc02179458d295f5049696
Zak Strassberg <zakstrassberg@gmail.com>
2019-12-17 23:23:10 -0500

    bot

--- packages/nexus_bot/src/index.ts
+++ packages/nexus_bot/src/index.ts
@@ -5,18 +5,19 @@ import {
   NewListingsSubscriptionSubscriptionVariables,
   NewListingsSubscriptionSubscription
 } from "./NewListingsSubscription.generated";
-import AWSAppSyncClient from "aws-appsync";
 import { createAuthLink } from "aws-appsync-auth-link";
 import { createSubscriptionHandshakeLink } from "aws-appsync-subscription-link";
 
 import { ApolloLink } from "apollo-link";
-import { createHttpLink } from "apollo-link-http";
 import ApolloClient, { ApolloQueryResult } from "apollo-client";
 import { InMemoryCache } from "apollo-cache-inmemory";
-import Observable from "zen-observable";
 import fetch from "node-fetch";
 import { AuthOptions } from "aws-appsync-auth-link";
 import ws from "ws";
+
+// TODO: whole bunch of cleanup. Uninstall unused node modules
+// Look at upgrading to Apollo 3
+
 [
   "DISCORD_TOKEN",
   "REACT_APP_APPSYNC_GRAPHQL_ENDPOINT",
@@ -27,49 +28,6 @@ import ws from "ws";
 });
 
 const discordClient = new Discord.Client();
-
-//////////////////////////////////////////////////
-// const globalAny: any = global;
-// globalAny.WebSocket = require("ws");
-// globalAny.window = globalAny.window || {
-//   setTimeout: setTimeout,
-//   clearTimeout: clearTimeout,
-//   WebSocket: globalAny.WebSocket,
-//   ArrayBuffer: global.ArrayBuffer,
-//   addEventListener: function() {},
-//   navigator: { onLine: true }
-// };
-// globalAny.localStorage = {
-//   store: {},
-//   getItem: function(key: any) {
-//     return this.store[key];
-//   },
-//   setItem: function(key: any, value: any) {
-//     this.store[key] = value;
-//   },
-//   removeItem: function(key: any) {
-//     delete this.store[key];
-//   }
-// };
-// require("es6-promise").polyfill();
-// require("isomorphic-fetch");
-
-// // If you want to use API key-based auth
-// const apiKey = process.env.REACT_APP_APPSYNC_GRAPHQL_ENDPOINT as string;
-
-// // Set up Apollo client
-// const gqlClient = new AWSAppSyncClient({
-//   url: process.env.REACT_APP_APPSYNC_GRAPHQL_ENDPOINT?.replace(
-//     "https",
-//     "wss"
-//   ).replace("appsync-api", "appsync-realtime-api") as string,
-//   region: process.env.AWS_REGION || "us-east-1",
-//   auth: {
-//     type: "API_KEY",
-//     apiKey
-//   }
-//   //   disableOffline: true //Uncomment for AWS Lambda
-// });
 
 (global as any).fetch = fetch;
 (global as any).WebSocket = ws;
@@ -110,8 +68,7 @@ discordClient.on("ready", () => {
   console.log(`Logged in as ${discordClient.user.tag}!`);
 
   const findAGameChannel = discordClient.channels.find(
-    // @ts-ignore
-    (_v, key) => key === "261581340862840843" || process.env.CHANNEL_ID
+    (_v, key) => key === (process.env.CHANNEL_ID || "261581340862840843")
   ) as Discord.TextChannel;
 
   console.log(`found channel '${findAGameChannel.name}'`);
